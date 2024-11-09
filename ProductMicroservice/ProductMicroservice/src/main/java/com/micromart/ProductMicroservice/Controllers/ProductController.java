b11544a3939c2df4d65af07e68caa9e0f9c664d5
tyagishubhangam <tyagishubhangam0090@gmail.com>
2024-11-10 00:02:11 +0530

    Implemented Gateway and resolved ProductMicroservice endpoints

--- ProductMicroservice/ProductMicroservice/src/main/java/com/micromart/ProductMicroservice/Controllers/ProductController.java
+++ ProductMicroservice/ProductMicroservice/src/main/java/com/micromart/ProductMicroservice/Controllers/ProductController.java
@@ -13,7 +13,7 @@ import java.util.List;
 @EnableWebMvc
 @RestController
 @CrossOrigin(origins = "http://127.0.0.1:5500")
-@RequestMapping("/api/micromart")
+@RequestMapping("/api/micromart/product")
 public class ProductController {
 
     ProductService productService;
