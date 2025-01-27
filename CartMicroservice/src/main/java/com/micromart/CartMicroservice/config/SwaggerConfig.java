5f32b4b7258ec61ae50700661179861763b61af5
Shubhangam Tyagi <tyagishubhangam0090@gmail.com>
2025-01-28 01:48:42 +0530

    Added swagger config

--- CartMicroservice/src/main/java/com/micromart/CartMicroservice/config/SwaggerConfig.java
+++ CartMicroservice/src/main/java/com/micromart/CartMicroservice/config/SwaggerConfig.java
@@ -1,8 +1,11 @@
 package com.micromart.CartMicroservice.config;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -22,7 +25,10 @@ public class SwaggerConfig implements WebMvcConfigurer {
     }
     @Bean
     public OpenAPI customOpenAPI() {
-        return new OpenAPI().info(
+        return new OpenAPI()
+                .addSecurityItem(new SecurityRequirement().addList("bearerAuth"))
+                .components(new Components().addSecuritySchemes("bearerAuth",
+                        new SecurityScheme().type(SecurityScheme.Type.HTTP).scheme("bearer").bearerFormat("JWT"))).info(
                 new Info().title("Cart-Micorservice-API").version("1.0")
         );
     }
