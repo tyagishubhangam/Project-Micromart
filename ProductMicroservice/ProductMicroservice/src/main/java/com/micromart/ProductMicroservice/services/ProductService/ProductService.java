d6c22d75871cbc277e16f192f24d1bbcac0d4e8a
Shubhangam Tyagi <tyagishubhangam0090@gmail.com>
2025-05-08 13:55:24 +0530

    Added Cloudinary for cloud storage of images

--- ProductMicroservice/ProductMicroservice/src/main/java/com/micromart/ProductMicroservice/services/ProductService/ProductService.java
+++ ProductMicroservice/ProductMicroservice/src/main/java/com/micromart/ProductMicroservice/services/ProductService/ProductService.java
@@ -2,9 +2,12 @@ package com.micromart.ProductMicroservice.ProductService;
 
 import com.micromart.ProductMicroservice.Product.Product;
 import com.micromart.ProductMicroservice.dto.ProductAddRequest;
+import com.micromart.ProductMicroservice.dto.ProductCard;
 import com.micromart.ProductMicroservice.dto.ProductWithReviewDto;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface ProductService {
     public void addProduct(ProductAddRequest product);
@@ -12,5 +15,7 @@ public interface ProductService {
     public ProductWithReviewDto getProductById(Long id);
     public boolean deleteProduct(Long id);
     public boolean updateProduct(Product product);
+    public String uploadProductImage(MultipartFile file) throws Exception;
+    public List<ProductCard> getAllProductCards();
 
 }
--- ProductMicroservice/ProductMicroservice/src/main/java/com/micromart/ProductMicroservice/services/ProductService/ProductServiceImpl.java
+++ ProductMicroservice/ProductMicroservice/src/main/java/com/micromart/ProductMicroservice/services/ProductService/ProductServiceImpl.java
@@ -58,24 +58,84 @@ import com.micromart.ProductMicroservice.Product.Product;
 import com.micromart.ProductMicroservice.ProductService.ProductService;
 import com.micromart.ProductMicroservice.clients.ReviewClient;
 import com.micromart.ProductMicroservice.dto.ProductAddRequest;
+import com.micromart.ProductMicroservice.dto.ProductCard;
 import com.micromart.ProductMicroservice.dto.ProductWithReviewDto;
+import com.micromart.ProductMicroservice.mappers.ProductCardMapper;
 import com.micromart.ProductMicroservice.mappers.ProductWithReviewMapper;
 import com.micromart.ProductMicroservice.repositories.ProductRepo;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Base64;
 import java.util.List;
 import java.util.Optional;
 @Service
 public class ProductServiceImpl implements ProductService {
-
+    private final String IMGBB_API_KEY = "db5884b66ef39291e15ade8a0896d890";
     public final ProductRepo productRepo;
     public final ProductWithReviewMapper productWithReviewMapper;
     public final ReviewClient reviewClient;
-    public ProductServiceImpl(ProductRepo productRepo, ProductWithReviewMapper productWithReviewMapper, ReviewClient reviewClient) {
+    private final ProductCardMapper productCardMapper;
+
+    public ProductServiceImpl(ProductRepo productRepo, ProductWithReviewMapper productWithReviewMapper, ReviewClient reviewClient, ProductCardMapper productCardMapper) {
         this.productRepo = productRepo;
         this.productWithReviewMapper = productWithReviewMapper;
         this.reviewClient = reviewClient;
+        this.productCardMapper = productCardMapper;
+    }
 
+    @Override
+    public String uploadProductImage(MultipartFile file)throws Exception {
+
+        String url = "https://api.imgbb.com/1/upload?key=" + IMGBB_API_KEY;
+        try{
+            byte[] bytes = file.getBytes();
+            String base64Image = Base64.getEncoder().encodeToString(bytes);
+            MultiValueMap<String, String> map = new LinkedMultiValueMap<>();
+            map.add("image", base64Image);
+            map.add("name", file.getOriginalFilename());
+
+            HttpHeaders headers = new HttpHeaders();
+            headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+            HttpEntity<MultiValueMap<String, String>> request = new HttpEntity<>(map, headers);
+
+            RestTemplate restTemplate = new RestTemplate();
+            ResponseEntity<String> response = restTemplate.postForEntity(url, request, String.class);
+            if (response.getStatusCode().is2xxSuccessful()) {
+                String responseBody = response.getBody();
+                System.out.println(responseBody);
+                // extract URL from JSON (simple way)
+                String imageUrl = responseBody.split("\"url\":\"")[1].split("\"")[0].replace("\\/", "/");
+                return imageUrl;
+            } else {
+                throw new RuntimeException("Image upload failed");
+            }
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+       return null;
+    }
+
+    @Override
+    public List<ProductCard> getAllProductCards() {
+        List<Product> products = productRepo.findAll();
+        List<ProductCard> productCards = new ArrayList<>();
+        for (Product product : products) {
+            ProductCard productCard = productCardMapper.mapProductCard(product);
+            productCards.add(productCard);
+        }
+        return productCards;
     }
 
     @Override
